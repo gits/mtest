c15403d767dc003a631459825256b5003823aac9
gits <noreply>
2014-01-17 09:48:47 -0800

    Add module b

---
+++ b.js
@@ -0,0 +1,16 @@
+/**
+ * @module b
+ * @requires montage/core/core
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class B
+ * @extends Montage
+ */
+exports.B = Montage.specialize(/** @lends B# */ {
+    constructor: {
+        value: function B() {
+            this.super();
+        }
+    }
+});
